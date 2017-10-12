ba5fa2322e145c02506c2219aeb6571a935a22d7
MIKORU <xiajing2204@gmail.com>
2017-10-12 14:11:28 +0800

    增加计算功能

--- pages/do_time/do_time.js
+++ pages/do_time/do_time.js
@@ -1,9 +1,49 @@
 Page({
   data: {
     index: 0,
-    gender: ["nan", "nv"]
+    gender: ["nan", "nv"],
+    score: 0,
+    remainingTime: 60,
+    num1: 0,
+    num2: 0,
+    ans:[0,0,0,0],
   },
-  
+  /**
+   * 求随机数 max为位数（10，100，10000）
+   */
+  range: function(max){
+    return Math.floor(Math.random()*100 % max);
+  },
+  printad: function() {
+    console.log(this.data.num1 +" "+ this.data.num2);
+  },
+  /**
+   * 初始化相加的数，和选择的数组
+   */
+  init: function(){
+    this.setData({
+      num1: this.range(10),
+      num2: this.range(10),
+    });
+    var arr = [this.range(10), this.range(10), this.range(10), this.range(10)];
+    var indexs = this.range(100) % 4;
+    arr[indexs] = this.data.num1 + this.data.num2;
+    this.setData({
+      ans: arr
+    });
+  },
+  /**
+   * 计算得分
+   */
+  touchAns: function(event){
+    var index = event.currentTarget.dataset.alphaBeta;
+    if (this.data.num1 + this.data.num2 === this.data.ans[index]){
+      this.setData({
+        score : this.data.score+1,
+      })
+    }
+    this.init();
+  },
   onLoad: function (options) {
     // Do some initialize when page load.
   },
@@ -12,6 +52,8 @@ Page({
   },
   onShow: function () {
     // Do something when page show.
+    this.init();
+    this.printad();
   },
   onHide: function () {
     // Do something when page hide.
